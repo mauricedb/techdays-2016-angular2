409445246771b0a3fc1f728c1f299d55b3096fd1
Maurice de Beijer <maurice.de.beijer@gmail.com>
2016-10-04 13:50:07 +0200

    Demo code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MovieComponent } from './movie/movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/movie.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class MovieService {
+
+  constructor(private http: Http) {
+  }
+
+  getMovies() {
+    return this.http
+      .get('/movies.json')
+      .map(rsp => rsp.json());
+  }
+}
